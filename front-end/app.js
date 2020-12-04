e16c8fc5d9117569b9f818b2860e318001748b88
Vi Tran <vitran86@icloud.com>
2020-12-04 17:10:49 -0500

    Revert "first commit"
    This reverts commit 5dc471d02fde2dc6cf9dd231c043a59555ae11b7.

--- front-end/app.js
+++ front-end/app.js
@@ -4,57 +4,71 @@ let serverURL = "";
 
 if (location.hostname === "localhost" || location.hostname === "127.0.0.1") {
   serverURL = "http://localhost:8000";
+} else {
+  serverURL = "https://afternoon-peak-58665.herokuapp.com";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
   // set up for search button
-  document.getElementById("add-btn").addEventListener("click", () => {
-    let arrCity = document.getElementById("arr-city-name").value.trim();
-    let depCity = document.getElementById("dep-city-name").value.trim();
-
-    console.log(depCity, arrCity);
-
-    //active the entry sections to show & post data
-    document.querySelector(".hero-box").classList.add("active");
-    //start to fetch data from server, then display it
-    postDataToServer(depCity, arrCity)
-      .then((data) => {
-        displayWeatherInfo(data); // ref. app-function.js
-        generateWeatherForecast(data);
-        //reset value in search boxes
-        document.querySelector("#departure-date").value = "";
-        document.querySelector("#arrival-date").value = "";
-        document.querySelector("#dep-city-name").value = "";
-        document.querySelector("#arr-city-name").value = "";
-      })
-      .catch((error) => {
-        console.log(`Error: ${error}`);
-      });
+  document.getElementById("search-btn").addEventListener("click", () => {
+    let searchTerm = document.getElementById("search-input").value;
+    //pass in search function if searchTerm is valid
+    if (searchTerm) {
+      search(searchTerm);
+      //active the entry sections to show & post data
+      document.querySelector(".hero-box").classList.add("active");
+      document.querySelector(".history-entry").classList.add("active");
+      //start to fetch data from server, then display it
+      postDataToServer(searchTerm)
+        .then((data) => {
+          displayWeatherInfo(data); // ref. app-function.js
+          postEntry(data);
+          //reset value in search boxes
+          document.querySelector("#search-input").value = "";
+          document.querySelector("#user-feeling").value = "";
+        })
+        .catch((error) => {
+          console.log(`Error: ${error}`);
+        });
+    }
   });
 
   // set up function to post data to server
-  const postDataToServer = async (depCity, arrCity) => {
-    /* let feeling = document.getElementById("user-feeling").value; */
+  const postDataToServer = async (searchTerm) => {
+    search(searchTerm);
+    let feeling = document.getElementById("user-feeling").value;
     const result = await fetch(`${serverURL}/addWeather`, {
       method: "POST",
       credentials: "same-origin",
       headers: {
         "Content-Type": "application/json",
-        // "Content-Type": "text/plain",
       },
       body: JSON.stringify({
-        depCity: depCity,
-        arrCity: arrCity,
+        method: searchMethod,
+        input: searchTerm,
+        mood: feeling,
       }),
     });
     if (result.status === 200) {
       const data = await result.json();
-      console.log(data);
       return data;
     } else {
       throw new Error(`Unable to fetch data`);
     }
   };
+
+  /* 
+// set up function to get data from server --> these codes use in case we want to get all data in projectData
+const getDataFromServer = async () => {
+const response = await fetch(`${serverURL}/allData`, {});
+if (response.status === 200) {
+  const allData = await response.json(); // allData = projectData in server.js
+  return allData;
+} else {
+  throw new Error(`Unable to fetch data`);
+}
+};
+*/
 
   // set up function to post data
   const postEntry = (data) => {
